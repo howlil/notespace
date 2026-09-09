355e601fc5e8cebb9b859a3d68ccc00a09440567
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-10 03:26:16 +0700

    refactor(canvas): use native laser glyph

--- apps/web/src/integrations/canvas/CanvasNativeIcons.tsx
+++ apps/web/src/integrations/canvas/CanvasNativeIcons.tsx
@@ -81,6 +81,10 @@ export function NativeMagicIcon(props: IconProps) {
   return <Icon strokeWidth={2} {...props}><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="m6 21 15-15-3-3-15 15 3 3M15 6l3 3" /><path d="M9 3a2 2 0 0 0 2 2 2 2 0 0 0-2 2 2 2 0 0 0-2-2 2 2 0 0 0 2-2M19 13a2 2 0 0 0 2 2 2 2 0 0 0-2 2 2 2 0 0 0-2-2 2 2 0 0 0 2-2" /></Icon>;
 }
 
+export function NativeLaserIcon(props: IconProps) {
+  return <Icon viewBox="0 0 20 20" strokeWidth={1.25} {...props}><g transform="rotate(90 10 10)"><path clipRule="evenodd" d="m9.644 13.69 7.774-7.773a2.357 2.357 0 0 0-3.334-3.334l-7.773 7.774L8 12l1.643 1.69Z" /><path d="m13.25 3.417 3.333 3.333M10 10l2-2M5 15l3-3M2.156 17.894l1-1M5.453 19.029l-.144-1.407M2.377 11.887l.866 1.118M8.354 17.273l-1.194-.758M.953 14.652l1.408.13" /></g></Icon>;
+}
+
 export function NativeLibraryIcon(props: IconProps) {
   return <Icon strokeWidth={1.25} {...props}><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 19a9 9 0 0 1 9 0 9 9 0 0 1 9 0M3 6a9 9 0 0 1 9 0 9 9 0 0 1 9 0M3 6v13M12 6v13M21 6v13" /></Icon>;
 }

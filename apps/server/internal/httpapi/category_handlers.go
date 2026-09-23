042e7aa1c8b4c0bca6961ef2d3284e89d8907bc8
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-23 12:29:42 +0700

    refactor: isolate category HTTP handlers

---
+++ apps/server/internal/httpapi/category_handlers.go
@@ -0,0 +1,67 @@
+package httpapi
+
+import "net/http"
+
+func (a API) listCategories(w http.ResponseWriter, r *http.Request) {
+	data, err := a.service.ListCategories(r.Context())
+	if err != nil {
+		fail(w, err)
+		return
+	}
+	send(w, 200, data)
+}
+
+func (a API) listCategoryWorkspaces(w http.ResponseWriter, r *http.Request) {
+	categoryID := r.PathValue("id")
+	query, err := workspaceQuery(r, categoryID)
+	if err != nil {
+		fail(w, err)
+		return
+	}
+	page, err := a.service.ListCategoryWorkspaces(r.Context(), categoryID, query)
+	if err != nil {
+		fail(w, err)
+		return
+	}
+	send(w, 200, page)
+}
+
+func (a API) createCategory(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		Title string `json:"title"`
+	}
+	if !decode(w, r, &body) {
+		return
+	}
+	category, err := a.service.CreateCategory(r.Context(), body.Title)
+	if err != nil {
+		fail(w, err)
+		return
+	}
+	w.Header().Set("Location", "/api/categories/"+category.ID)
+	send(w, 201, category)
+}
+
+func (a API) updateCategory(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		Title string `json:"title"`
+	}
+	if !decode(w, r, &body) {
+		return
+	}
+	category, err := a.service.UpdateCategory(r.Context(), r.PathValue("id"), body.Title)
+	if err != nil {
+		fail(w, err)
+		return
+	}
+	send(w, 200, category)
+}
+
+func (a API) deleteCategory(w http.ResponseWriter, r *http.Request) {
+	if err := a.library.DeleteCategory(r.Context(), r.PathValue("id")); err != nil {
+		fail(w, err)
+		return
+	}
+	send(w, http.StatusNoContent, nil)
+}
+

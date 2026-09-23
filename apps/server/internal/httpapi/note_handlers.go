8ae2500cf8feb8ae81735668abe0d1f3faf5ef58
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-23 12:29:45 +0700

    refactor: isolate note HTTP handlers

---
+++ apps/server/internal/httpapi/note_handlers.go
@@ -0,0 +1,47 @@
+package httpapi
+
+import (
+	"net/http"
+
+	"github.com/howlil/notespace/apps/server/internal/workspace"
+)
+
+func (a API) createNote(w http.ResponseWriter, r *http.Request) {
+	var body workspace.NoteCreate
+	if !decode(w, r, &body) {
+		return
+	}
+	note, err := a.service.CreateNote(r.Context(), r.PathValue("id"), body)
+	if err != nil {
+		fail(w, err)
+		return
+	}
+	send(w, http.StatusCreated, note)
+}
+
+func (a API) updateNote(w http.ResponseWriter, r *http.Request) {
+	var body workspace.NoteUpdate
+	if !decode(w, r, &body) {
+		return
+	}
+	note, err := a.service.UpdateNote(r.Context(), r.PathValue("id"), r.PathValue("noteId"), body)
+	if err != nil {
+		fail(w, err)
+		return
+	}
+	send(w, http.StatusOK, note)
+}
+
+func (a API) deleteNote(w http.ResponseWriter, r *http.Request) {
+	version, err := expectedVersion(r)
+	if err != nil || version == nil {
+		fail(w, workspace.ErrInvalid)
+		return
+	}
+	if err := a.service.DeleteNote(r.Context(), r.PathValue("id"), r.PathValue("noteId"), *version); err != nil {
+		fail(w, err)
+		return
+	}
+	send(w, http.StatusNoContent, nil)
+}
+
